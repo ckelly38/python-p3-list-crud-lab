bf30eaa34b56bcd25ccadc62c58fd3a6a9b42780
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 21:01:19 -0600

    this demonstrates how to manage lists

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,46 @@
+import random;
 def create_an_empty_list():
-    return None
+    return [];
 
 def create_a_list():
-    return None
+    return [int(random.random() * 10) for i in range(4)];
 
 def add_element_to_end_of_list(l, element):
-    return None
+    if (l == None): return [element];
+    else:
+        l.append(element);
+        return l;
 
 def add_element_to_start_of_list(l, element):
-    return None
+    if (l == None): return [element];
+    else:
+        l.insert(0, element);
+        return l;
 
 def remove_element_from_end_of_list(l):
-    return None
+    if (l == None): return None;
+    else:
+        l.pop();
+        return l;
 
 def remove_element_from_start_of_list(l):
-    return None
+    if (l == None): return None;
+    elif (len(l) < 1): return [];
+    else:
+        l.remove(l[0]);
+        return l;
 
 def retrieve_first_element_from_list(l):
-    return None
+    if (l == None or len(l) < 1): return None;
+    else: return l[0];
 
 def retrieve_element_from_index(l, index):
-    return None
+    if (l == None or len(l) < 1): return None;
+    elif (index < 0 or index > len(l) - 1): raise Exception("invalid index found and used here!");
+    else: return l[index];
 
 def retrieve_last_element_from_list(l):
-    return None
+    if (l == None or len(l) < 1): return None;
+    else: return l[len(l) - 1];
+
+#print(create_a_list());
